7d6870b93a8c1d8925f6ea4d36884d546b9070db
kauassilva <kauassilva1@gmail.com>
2024-08-17 17:29:02 -0300

    Feat: create a markdown document for Solution3190

--- src/test/java/com/kauassilva/algorithms/solutions/Solution3190Test.java
+++ src/test/java/com/kauassilva/algorithms/solutions/Solution3190Test.java
@@ -3,28 +3,9 @@ package com.kauassilva.algorithms.solutions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * <p>
- * You are given an integer array {@code nums}. In one operation, you can add
- * or subtract 1 from <b>any</b> element of {@code nums}.
- *
- * <p>
- * Return the <b>minimum</b> number of operations to make all elements of {@code nums} divisible by 3.
- *
- * <p>
- * <b>Constraints:</b>
- * <ul>
- *     <li>{@code 1 <= nums.length <= 50}</li>
- *     <li>{@code 1 <= nums[i] <= 50}</li>
- * </ul>
- *
- * @see <a href="https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/">
- *     3190. Find Minimum Operations to Make All Elements Divisible by Three</a>
- */
 class Solution3190Test {
 
     @Test
